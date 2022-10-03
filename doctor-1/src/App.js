c98867b4fa0c6dad74b7fde4f607e38a0ec52b14
brigide <brigidegd@gmail.com>
2022-10-03 19:55:17 -0300

    first

---
+++ doctor-1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes } from './routes'
+import { ResetCss } from "./styles/reset-css";
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <ResetCss />
+    </>
+  );
+}
+
+export default App;
---
+++ doctor-1/src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, BrowserRouter, Routes as Switch } from 'react-router-dom';
+
+export const Routes = () => (
+    <BrowserRouter basename="/">
+        <Switch>
+        </Switch>
+    </BrowserRouter>
+);
